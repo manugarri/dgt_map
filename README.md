58906b0cf9cd10740e5bbf7c5845ecb026c929cc
manuel <manuel.garrido.pena@gmail.com>
2021-01-23 11:25:04 +0000

    comment leaflet providers

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # A better Road Conditions map for Spain
 
 [Blog post](http://blog.manugarri.com/making-a-simple-better-road-conditions-map-for-spain/)
-You can see the map live [here](http://dgt.manugarri.com) 
+You can see the map live [here](http://dgt.manugarri.com)
 
 ![the glory!](https://i.imgur.com/2uMnyqi.png)
+
+
+On unix, to set up the project locally , run `make setup`,  to run it, `make run`.
 
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import os
 from pathlib import Path
 
-from fastapi import FastAPI,Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import httpx
@@ -10,7 +9,7 @@ from openrouteservice import client
 from openrouteservice.geocode import pelias_search as geocode
 
 OPENROUTESERVICE_TOKEN = os.environ['OPENROUTESERVICE_TOKEN']
-DGT_URL = 'http://infocar.dgt.es/etraffic/BuscarElementos?latNS=44&longNS=5&latSW=27&longSW=-19&zoom=6&accion=getElementos&Camaras=true&SensoresTrafico=true&SensoresMeteorologico=true&Paneles=true&Radares=true&IncidenciasRETENCION=true&IncidenciasOBRAS=false&IncidenciasMETEOROLOGICA=true&IncidenciasPUERTOS=true&IncidenciasOTROS=true&IncidenciasEVENTOS=true&IncidenciasRESTRICCIONES=true&niveles=true&caracter=acontecimiento';
+DGT_URL = 'http://infocar.dgt.es/etraffic/BuscarElementos?latNS=44&longNS=5&latSW=27&longSW=-19&zoom=6&accion=getElementos&Camaras=true&SensoresTrafico=true&SensoresMeteorologico=true&Paneles=true&Radares=true&IncidenciasRETENCION=true&IncidenciasOBRAS=false&IncidenciasMETEOROLOGICA=true&IncidenciasPUERTOS=true&IncidenciasOTROS=true&IncidenciasEVENTOS=true&IncidenciasRESTRICCIONES=true&niveles=true&caracter=acontecimiento'
 
 
 openroute = client.Client(key=OPENROUTESERVICE_TOKEN)
@@ -22,14 +21,7 @@ app.mount(
 )
 
 templates = Jinja2Templates(directory="templates")
-"""
-#with open('index.html') as fname:
-#    index_html_content = fname.read()
-@app.get("/", response_class=HTMLResponse)
-async def index():
-    return HTMLResponse(content=index_html_content, status_code=200)
 
-"""
 
 @app.get("/")
 async def root(request: Request):
@@ -37,13 +29,15 @@ async def root(request: Request):
         "index.html", {"request": request}
     )
 
+
 @app.get("/events")
 async def get_dgt_events():
     return httpx.get(DGT_URL).json()
 
 
 def geo(geo_text):
-    return geocode(openroute, geo_text)['features'][0]['geometry']['coordinates']  
+    return geocode(openroute, geo_text)['features'][0]['geometry']['coordinates']
+
 
 @app.get("/geo")
 async def get_geo(geo_text):
@@ -54,10 +48,12 @@ async def get_geo(geo_text):
 async def get_route(origin_text, destination_text):
     origin = geo(origin_text)
     destination = geo(destination_text)
-    request_params = {'coordinates': [origin, destination],
-                'format_out': 'geojson',
-                'profile': 'driving-car',
-                'preference': 'shortest',
-                'instructions': 'false',}
+    request_params = {
+        'coordinates': [origin, destination],
+        'format_out': 'geojson',
+        'profile': 'driving-car',
+        'preference': 'shortest',
+        'instructions': 'false',
+    }
     route = openroute.directions(**request_params)
     return route
